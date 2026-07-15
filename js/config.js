7ea4cd77ec16f6eb08ca941b313ed64c473d2fd9
farah24g <ra7i2005@gmail.com>
2026-07-15 18:23:10 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const MawreadConfig = {
+    siteName: "مورد الوظائف الذكي",
+    brandName: "Mawread Jobs Intelligence",
+    siteURL: "https://jobs.mawread.com",
+    defaultLanguage: "ar",
+    version: "1.1.0",
+    storageKey: "mawread_recent_searches"
+};
