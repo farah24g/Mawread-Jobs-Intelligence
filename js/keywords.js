3e7894a8e19db659e5554111cb1669e6db7dcfca
farah24g <ra7i2005@gmail.com>
2026-07-15 18:24:04 +0300

    Create keywords.js

---
+++ js/keywords.js
@@ -0,0 +1,8 @@
+const JobKeywords = {
+    "مبرمج": ["Developer", "Software Engineer", "Programmer", "مطور برمجيات"],
+    "محاسب": ["Accountant", "Finance", "Accounting", "محاسبة"],
+    "تسويق": ["Marketing", "Digital Marketing", "تسويق الكتروني"],
+    "تصميم": ["Designer", "Graphic Designer", "مصمم"],
+    "مبيعات": ["Sales", "Sales Representative", "مبيعات"],
+    "تقنية معلومات": ["Information Technology", "IT Specialist", "تقنية معلومات"]
+};
