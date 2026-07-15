166554b6d6ee872c4414a6083188c51f867e339e
farah24g <ra7i2005@gmail.com>
2026-07-15 18:20:51 +0300

    Create providers.js

---
+++ js/providers.js
@@ -0,0 +1,22 @@
+const JobProviders = {
+    engines: [
+        {
+            name: "LinkedIn Jobs",
+            reason: "الأفضل للبحث الذكي والروابط المباشرة للشركات",
+            url: "https://www.google.com/search",
+            queryPrefix: "site:linkedin.com/jobs"
+        },
+        {
+            name: "Indeed & Glassdoor",
+            reason: "أضخم منصات التوظيف العامة والعالمية في صفحة واحدة",
+            url: "https://www.google.com/search",
+            queryPrefix: "site:indeed.com/q- OR site:glassdoor.com"
+        },
+        {
+            name: "Bayt & GulfTalent",
+            reason: "البحث المتخصص للشركات والهيئات في الخليج والشرق الأوسط",
+            url: "https://www.google.com/search",
+            queryPrefix: "site:bayt.com/ar/jobs OR site:gulftalent.com"
+        }
+    ]
+};
