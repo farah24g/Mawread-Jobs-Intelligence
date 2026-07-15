267f746f793ea2afab132805c776ea201bdf3268
farah24g <ra7i2005@gmail.com>
2026-07-15 20:04:52 +0300

    Update keywords.js

--- js/keywords.js
+++ js/keywords.js
@@ -1,8 +1,27 @@
-const JobKeywords = {
-    "مبرمج": ["Developer", "Software Engineer", "Programmer", "مطور برمجيات"],
-    "محاسب": ["Accountant", "Finance", "Accounting", "محاسبة"],
-    "تسويق": ["Marketing", "Digital Marketing", "تسويق الكتروني"],
-    "تصميم": ["Designer", "Graphic Designer", "مصمم"],
-    "مبيعات": ["Sales", "Sales Representative", "مبيعات"],
-    "تقنية معلومات": ["Information Technology", "IT Specialist", "تقنية معلومات"]
+// js/keywords.js
+
+const keywords = {
+    "محاسب": "Accountant",
+    "مدير مشاريع": "Project Manager",
+    "مهندس برمجيات": "Software Engineer",
+    "مطور ويب": "Web Developer",
+    "مصمم جرافيك": "Graphic Designer",
+    "مسوق رقمي": "Digital Marketer",
+    "مترجم": "Translator",
+    "كاتب محتوى": "Content Writer",
+    "ممثل خدمة عملاء": "Customer Service Representative",
+    "مدير مبيعات": "Sales Manager",
+    "محلل بيانات": "Data Analyst",
+    "مسؤول موارد بشرية": "HR Specialist",
+    "مهندس مدني": "Civil Engineer",
+    "مهندس كهرباء": "Electrical Engineer",
+    "طبيب": "Doctor",
+    "ممرض": "Nurse",
+    "صيدلي": "Pharmacist",
+    "مدرس": "Teacher",
+    "سائق": "Driver",
+    "أمين مستودع": "Warehouse Keeper"
+    // يمكنك إضافة أي مسميات وظيفية أخرى هنا بنفس الهيكل
 };
+
+export default keywords;
