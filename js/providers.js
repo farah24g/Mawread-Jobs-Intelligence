a4b9b3f4f44b9fce1abe3d8501dfa155e7ee9f4b
farah24g <ra7i2005@gmail.com>
2026-07-16 20:03:27 +0300

    Update providers.js

--- js/providers.js
+++ js/providers.js
@@ -1,43 +1,73 @@
-// js/providers.js
-
-window.MawreadSearchEngine = {
-    PROVIDERS: {
-        linkedin: {
-            name: "LinkedIn",
-            baseUrl: "https://www.linkedin.com/jobs/search",
-            locationNames: { global: "", egypt: "Egypt", saudi: "Saudi Arabia", uae: "United Arab Emirates" },
-            buildUrl: function(p) {
-                const q = encodeURIComponent(p.keyword);
-                const loc = encodeURIComponent(this.locationNames[p.country] || "");
-                return `${this.baseUrl}/?keywords=${q}&location=${loc}`;
-            }
+// js/providers-v2.js
+window.JobProviders = {
+    indeed: {
+        name: "Indeed",
+        domains: {
+            global: "https://www.indeed.com/jobs",
+            egypt: "https://eg.indeed.com/jobs",
+            saudi: "https://sa.indeed.com/jobs",
+            uae: "https://ae.indeed.com/jobs",
+            qatar: "https://qa.indeed.com/jobs",
+            kuwait: "https://kw.indeed.com/jobs"
         },
-        indeed: {
-            name: "Indeed",
-            domains: { global: "www.indeed.com", egypt: "eg.indeed.com", saudi: "sa.indeed.com", uae: "ae.indeed.com" },
-            buildUrl: function(p) {
-                const q = encodeURIComponent(p.keyword);
-                const domain = this.domains[p.country] || this.domains.global;
-                return `https://${domain}/jobs?q=${q}`;
-            }
+        locationNames: {
+            global: "",
+            egypt: "Egypt",
+            saudi: "Saudi Arabia",
+            uae: "United Arab Emirates",
+            qatar: "Qatar",
+            kuwait: "Kuwait"
         },
-        bayt: {
-            name: "Bayt.com",
-            baseUrl: "https://www.bayt.com/en",
-            countryPaths: { global: "international/jobs", egypt: "egypt/jobs", saudi: "saudi-arabia/jobs", uae: "uae/jobs" },
-            buildUrl: function(p) {
-                const path = this.countryPaths[p.country] || this.countryPaths.global;
-                const kw = encodeURIComponent(p.keyword_ar);
-                return `${this.baseUrl}/${path}/${kw}-jobs/`;
-            }
+        buildUrl: function(params) {
+            const baseUrl = this.domains[params.country] || this.domains.global;
+            const queryParams = [];
+            if (params.keyword) queryParams.push(`q=${encodeURIComponent(params.keyword)}`);
+            const loc = this.locationNames[params.country];
+            if (loc) queryParams.push(`l=${encodeURIComponent(loc)}`);
+            if (params.workplace === "remote") queryParams.push("sc=0kf%3Aattr(DS79X)%3B");
+            if (params.datePosted === "24h") queryParams.push("fromage=1");
+            return `${baseUrl}?${queryParams.join("&")}`;
         }
     },
 
-    generateResults: function(params) {
-        return Object.values(this.PROVIDERS).map(provider => ({
-            name: provider.name,
-            url: provider.buildUrl(params)
-        }));
+    linkedin: {
+        name: "LinkedIn",
+        baseUrl: "https://www.linkedin.com/jobs/search",
+        locationNames: {
+            global: "",
+            egypt: "Egypt",
+            saudi: "Saudi Arabia",
+            uae: "United Arab Emirates",
+            qatar: "Qatar",
+            kuwait: "Kuwait"
+        },
+        buildUrl: function(params) {
+            const queryParams = [];
+            if (params.keyword) queryParams.push(`keywords=${encodeURIComponent(params.keyword)}`);
+            const loc = this.locationNames[params.country];
+            if (loc) queryParams.push(`location=${encodeURIComponent(loc)}`);
+            if (params.workplace === "remote") queryParams.push("f_WT=2");
+            if (params.datePosted === "24h") queryParams.push("f_TPR=r86400");
+            return `${this.baseUrl}?${queryParams.join("&")}`;
+        }
+    },
+
+    bayt: {
+        name: "Bayt.com",
+        baseUrl: "https://www.bayt.com/en",
+        countryPaths: {
+            global: "international/jobs",
+            egypt: "egypt/jobs",
+            saudi: "saudi-arabia/jobs",
+            uae: "uae/jobs",
+            qatar: "qatar/jobs",
+            kuwait: "kuwait/jobs"
+        },
+        buildUrl: function(params) {
+            const path = this.countryPaths[params.country] || this.countryPaths.global;
+            let url = `${this.baseUrl}/${path}/`;
+            if (params.keyword_ar) url += `${encodeURIComponent(params.keyword_ar)}-jobs/`;
+            return url;
+        }
     }
 };
-Object.freeze(window.MawreadSearchEngine);
